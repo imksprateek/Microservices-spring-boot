b4f506359abfe70ac0558e9f7630ccac77bc5932
imksprateek <ksprateek2004@gmail.com>
2024-09-12 20:43:37 +0530

    Initialized spring Microservices project

---
+++ employee-service/src/main/java/studio/ksprateek/employeeapp/employeeservice/EmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package studio.ksprateek.employeeapp.employeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+}
